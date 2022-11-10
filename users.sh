6f5b8e374c309794943dc1ef4966c4f3b8c93642
Cameron Griffin (He/Him) <101752687+CodrCam@users.noreply.github.com>
2022-11-09 18:28:34 -0800

    Update users.sh

--- users.sh
+++ users.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+#Script: Ops 201 Class 03 Ops Challenge Solution
+#Author: Cameron Griffin
+#Date of latest revision: 11-9-2022
+#Purpose: Print a last logins
+#But it doesn't do anything
+
+
+#Main
+users_login() { 
+        echo ~$last 
+}
+
+
+#End
